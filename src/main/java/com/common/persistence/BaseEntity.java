ea7a4be936b8e62254599885d92756f50ecf0714
zhouxiaoyuan <zhouxiaoyuanyouxi@126.com>
2016-04-28 13:20:56 +0800

    base

---
+++ src/main/java/com/common/persistence/BaseEntity.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright &copy; 2012-2014 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
+ */
+package com.common.persistence;
+
+import java.io.Serializable;
+
+/**
+ * Entity支持类
+ * @author ThinkGem
+ * @version 2014-05-16
+ */
+public abstract class BaseEntity<T> implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 实体编号（唯一标识）
+	 */
+	protected String id;
+	
+	
+	
+}
---
+++ src/main/java/com/common/persistence/DataEntity.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright &copy; 2012-2014 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
+ */
+package com.common.persistence;
+
+/**
+ * 数据Entity类
+ * @author ThinkGem
+ * @version 2014-05-16
+ */
+public abstract class DataEntity<T> extends BaseEntity<T> {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+}
